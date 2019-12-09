9c80ea362f140604390e83ff3b08b071a77c8926
Victor Cisneiros <cisneiros.work@gmail.com>
2019-12-09 20:23:11 +0000

    create user

--- src/main/java/com/cisneiros/prova/UserController.java
+++ src/main/java/com/cisneiros/prova/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,10 @@ public class UserController {
 	@GetMapping("/utilizadores")
 	ResponseEntity<List<User>> getAllUsers() {
 		return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/utilizador")
+	ResponseEntity<User> create(@RequestBody User user) {
+	    return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
 	}
 }
